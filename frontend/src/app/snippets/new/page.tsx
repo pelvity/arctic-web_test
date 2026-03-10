fe255e38a68f0795e71cb2763021347a667bbf70
pelvity <pelvity@gmail.com>
2026-03-10 12:32:06 +0100

    chore: remove node_modules from git tracking

--- frontend/src/app/snippets/new/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SnippetForm } from "@/components/SnippetForm";
-
-export default function NewSnippetPage() {
-    return (
-        <div className="py-8">
-            <SnippetForm isEdit={false} />
-        </div>
-    );
-}
