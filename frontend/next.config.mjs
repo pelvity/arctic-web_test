70af1b8ecd2063a589a06987831ddf1bfe0af282
pelvity <pelvity@gmail.com>
2026-03-11 11:27:28 +0100

    feat: deploy to vercel and aws via ecr

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -7,6 +7,9 @@ const withNextIntl = createNextIntlPlugin(
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default withNextIntl(nextConfig);
